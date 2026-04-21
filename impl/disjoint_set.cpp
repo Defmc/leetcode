1913d4b2449e749d8eda87e21814ae3662fcf2e9
Defmc <defmc0101@gmail.com>
2026-04-21 19:52:24 -0300

    feat: add disjoint set union data structure

---
+++ impl/disjoint_set.cpp
@@ -0,0 +1,29 @@
+#include <numeric>
+#include <vector>
+
+class disjoint_set {
+  std::vector<size_t> parent, size;
+
+public:
+  disjoint_set(const size_t n) : parent(n), size(n, 1) {
+    std::iota(parent.begin(), parent.end(), 0);
+  }
+
+  size_t find(const size_t v) {
+    if (v != parent[v])
+      parent[v] = find(parent[v]);
+    return parent[v];
+  }
+
+  bool unite(const size_t a, const size_t b) {
+    size_t fs_a = find(a), fs_b = find(b);
+    if (fs_a != fs_b) {
+      if (size[fs_a] < size[fs_b])
+        std::swap(fs_a, fs_b);
+      parent[fs_b] = fs_a;
+      size[fs_a] += size[fs_b];
+      return true;
+    }
+    return false;
+  }
+};
