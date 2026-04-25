59a74c45a34dec31f3e146c156ec362704b66849
Defmc <defmc0101@gmail.com>
2026-04-25 12:14:15 -0300

    fix: improve memory complexity, worse cache optimisation

--- 6.cpp
+++ 6.cpp
@@ -1,29 +1,25 @@
 #include <string>
-#include <vector>
 
 using namespace std;
 
+// worse cache behaviour than doing the pointer bouncing, but better in terms of
+// allocation
 class Solution {
 public:
   string convert(string s, int numRows) {
-    std::vector<std::string> rows(numRows);
-    int p = 0, d = 1;
-    for (size_t i = 0; i < s.size(); i++) {
-      rows[p].push_back(s[i]);
-      p += d;
-      if (p + 1 > numRows) {
-        d = -1;
-        p = std::max(0, numRows - 2);
-      } else if (p < 0) {
-        d = 1;
-        p = std::min(1, numRows - 1);
+    if (numRows == 1)
+      return s;
+    std::string res(s.size(), ' ');
+    for (size_t row = 0; row < numRows; row++) {
+      bool lastBigJump = false;
+      for (size_t i = row; i < s.size();) {
+        res[i] = s[i];
+        i += ((row > 0 && lastBigJump) || row == numRows - 1)
+                 ? 2 * row
+                 : 2 * (numRows - 1 - row);
+        lastBigJump = !lastBigJump;
       }
     }
-    std::string res;
-    for (auto &str : rows) {
-      res += str;
-    }
-
     return res;
   }
 };
